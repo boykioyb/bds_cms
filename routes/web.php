70cb63db9072343d58f352e4a18f700910e3827a
boykioyb <boykioyb96@gmail.com>
2018-10-25 23:16:55 +0700

    chia layouts

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/docs', function () {
+    return view('docs');
 });
+Route::get('/', function () {
+    return view('home.index');
+});
+Route::get('/sliders','SliderController@index')->name('sliders');
